858a80f4a0116686b241eba3fbd60676c67e4f11
Justin Wheeler <justinwwheeler@gmail.com>
2021-08-07 20:19:55 +0000

    updates for deposit and account classes

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
+}
 
-  constructor(amount) {
+class Deposit {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.amount;
   }
-
 }
 
+class Withdrawal {
 
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+  commit() {
+    this.account.balance -= this.amount;
+  }
+}
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('snow-patrol');
+
+t1 = new Deposit(120.00, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(50.25, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Withdrawal(9.99, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+
+console.log('Balance:', myAccount.balance);
